d28b1f6471a2de17bb26ee856dac5c3bb7722c41
D N <4661784+retyui@users.noreply.github.com>
2023-11-14 01:01:00 +0100

    New E2E pipeline

--- scripts/android_run_e2e.sh
+++ scripts/android_run_e2e.sh
@@ -9,10 +9,10 @@ adb shell screenrecord /sdcard/video_record.mp4 & echo $! > video_record.pid
 sleep 3
 
 # Retry 3 times before the steps actually fails
-(echo "===== Install node_modules Attempt:  1 ====" && $HOME/.maestro/bin/maestro test .maestro/ -e APP_ID=$RN_ANDROID_APP_ID -e IS_ANDROID=true -e IS_CI=true --format junit --output report1.xml) || \
-(echo "===== Install node_modules Attempt:  2 ====" && sleep 20 && $HOME/.maestro/bin/maestro test .maestro/ -e APP_ID=$RN_ANDROID_APP_ID -e IS_ANDROID=true -e IS_CI=true --format junit --output report2.xml) || \
-(echo "===== Install node_modules Attempt:  3 ====" && sleep 60 && $HOME/.maestro/bin/maestro test .maestro/ -e APP_ID=$RN_ANDROID_APP_ID -e IS_ANDROID=true -e IS_CI=true --format junit --output report3.xml) || \
-(echo "===== Install node_modules Step Failed ====" && touch "$TMP_FILE")
+(echo "===== Run E2E Attempt:  1 ====" && $HOME/.maestro/bin/maestro test .maestro/ -e APP_ID=$RN_ANDROID_APP_ID -e IS_ANDROID=true -e IS_CI=true --format junit --output report1.xml) || \
+(echo "===== Run E2E Attempt:  2 ====" && sleep 20 && $HOME/.maestro/bin/maestro test .maestro/ -e APP_ID=$RN_ANDROID_APP_ID -e IS_ANDROID=true -e IS_CI=true --format junit --output report2.xml) || \
+(echo "===== Run E2E Attempt:  3 ====" && sleep 60 && $HOME/.maestro/bin/maestro test .maestro/ -e APP_ID=$RN_ANDROID_APP_ID -e IS_ANDROID=true -e IS_CI=true --format junit --output report3.xml) || \
+(echo "===== Run E2E Step Failed ====" && touch "$TMP_FILE")
 
 # Stop video record process
 kill -SIGINT "$(cat video_record.pid)"
